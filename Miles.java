deb946712a726449687c6df5cd27c49484d6296f
SamojlovaA <samojlovaanna@icloud.com>
2021-01-08 19:30:31 +0400

    Miles

---
+++ Miles.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ticketPrice = 30000;
+        int mile = 20;
+        int numberOfMiles =  ticketPrice / mile;
+        System.out.println(numberOfMiles);
+    }
+}
